9144730ebbca48988492b462053a5839e7233217
Santiago <santiperez20048@gmail.com>
2025-06-14 20:35:43 -0300

    Aplicando .gitignore para ignorar archivos temporales y binarios

---
+++ FinanzasApp/FinanzasApp.API/Controllers/FinanzasController.cs
@@ -0,0 +1,38 @@
+﻿using FinanzasApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using FinanzasApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanzasApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FinanzasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FinanzasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var movimientos = await _context.Movimientos.ToListAsync();
+            return Ok(movimientos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Movimiento movimiento)
+        {
+            if (movimiento == null)
+                return BadRequest();
+
+            _context.Movimientos.Add(movimiento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = movimiento.Id }, movimiento);
+        }
+    }
+}
