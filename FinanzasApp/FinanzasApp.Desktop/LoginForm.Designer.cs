5873e64cfb6dbe5096b5f68ec76ac36c71802c15
Santiago <santiperez20048@gmail.com>
2025-06-15 20:55:36 -0300

    fix: Arregle la estetica del formulario de login  y agregue un icono.

--- FinanzasApp/FinanzasApp.Desktop/LoginForm.Designer.cs
+++ FinanzasApp/FinanzasApp.Desktop/LoginForm.Designer.cs
@@ -28,17 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             label1 = new Label();
             txtUsuario = new TextBox();
             txtPassword = new TextBox();
             label2 = new Label();
             btnIngresar = new Button();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(71, 164);
+            label1.Location = new Point(26, 142);
             label1.Name = "label1";
             label1.Size = new Size(50, 15);
             label1.TabIndex = 0;
@@ -46,23 +50,23 @@
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(127, 161);
+            txtUsuario.Location = new Point(82, 139);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.Size = new Size(130, 23);
             txtUsuario.TabIndex = 1;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(127, 190);
+            txtPassword.Location = new Point(82, 168);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(100, 23);
+            txtPassword.Size = new Size(130, 23);
             txtPassword.TabIndex = 2;
             txtPassword.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(51, 193);
+            label2.Location = new Point(6, 171);
             label2.Name = "label2";
             label2.Size = new Size(70, 15);
             label2.TabIndex = 3;
@@ -70,7 +74,7 @@
             // 
             // btnIngresar
             // 
-            btnIngresar.Location = new Point(140, 250);
+            btnIngresar.Location = new Point(107, 217);
             btnIngresar.Name = "btnIngresar";
             btnIngresar.Size = new Size(75, 23);
             btnIngresar.TabIndex = 4;
@@ -78,19 +82,44 @@
             btnIngresar.UseVisualStyleBackColor = true;
             btnIngresar.Click += btnIngresar_Click;
             // 
+            // label3
+            // 
+            label3.Font = new Font("Adobe Fan Heiti Std B", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(47, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(192, 27);
+            label3.TabIndex = 4;
+            label3.Text = "Ingreso al Sistema";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.Image = Properties.Resources.PikPng_com_twitter_icons_png_2837538;
+            pictureBox1.Location = new Point(95, 39);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(87, 70);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(444, 450);
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(276, 286);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
             Controls.Add(btnIngresar);
             Controls.Add(label2);
             Controls.Add(txtPassword);
             Controls.Add(txtUsuario);
             Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "LoginForm";
-            Text = "LoginForm";
+            Text = "Login";
             Load += LoginForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -102,5 +131,7 @@
         private TextBox txtPassword;
         private Label label2;
         private Button btnIngresar;
+        private Label label3;
+        private PictureBox pictureBox1;
     }
 }
